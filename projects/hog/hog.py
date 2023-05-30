f022317f26fd337fec00b2506368c8db5eef50db
notonlyericy <springdeepinside@gmail.com>
2023-05-30 13:46:18 +0800

    hog phase2

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -149,12 +149,12 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
         else:
             score1, score0, last_turn_score1 = cal(score1, score0, last_turn_score1, strategy1, dice, feral_hogs)
         who = other(who)
-
-    # END PROBLEM 5
-    # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
-    # BEGIN PROBLEM 6
-    "*** YOUR CODE HERE ***"
-    # END PROBLEM 6
+        # END PROBLEM 5
+        # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
+        # BEGIN PROBLEM 6
+        "*** YOUR CODE HERE ***"
+        say = say(score0, score1)
+        # END PROBLEM 6
     return score0, score1
 
 
@@ -240,6 +240,20 @@ def announce_highest(who, last_score=0, running_high=0):
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     # BEGIN PROBLEM 7
     "*** YOUR CODE HERE ***"
+    def say(score0, score1):
+        score = 0
+        if who == 0:
+            score = score0
+        else:
+            score = score1
+        
+        gain = score - last_score
+        if gain > running_high:
+            print(gain, "point(s)! That's the biggest gain yet for Player", who)
+        else:
+            gain = running_high
+        return announce_highest(who, score, gain)
+    return say
     # END PROBLEM 7
 
 
